dafc2f55479d76f816916c532382a056ca3cb8f5
Ayoub <ayoubaouinti1990@gmail.com>
2024-02-19 16:56:16 +0100

    Enhancements

--- app/Http/Controllers/BlogController.php
+++ app/Http/Controllers/BlogController.php
@@ -19,10 +19,7 @@ class BlogController extends Controller
     /**
      * Display a listing of the resource.
      */
-    public function index()
-    {
-        //
-    }
+    
 
     /**
      * Show the form for creating a new resource.
--- routes/web.php
+++ routes/web.php
@@ -24,9 +24,6 @@ Route::controller(ThemeController::class)->name('theme.')->group(function () {
     Route::get('/', 'index')->name('index');
     Route::get('/category/{id}', 'category')->name('category');
     Route::get('/contact', 'contact')->name('contact');
-    Route::get('/single-blog', 'singleBlog')->name('singleBlog');
-    // Route::get('/login', 'login')->name('login');
-    // Route::get('/register', 'register')->name('register');
 });
 
 
@@ -38,25 +35,10 @@ Route::post('/contact/store', [ContactController::class, 'store'])->name('contac
 
 //Blog Route
 Route::get('/my-blogs', [BlogController::class,'myBlogs'])->name('blogs.my-blogs');
-Route::resource('blogs', BlogController::class);
+Route::resource('blogs', BlogController::class)->except('index');
 
 
 //comment route
 Route::post('/comment/store', [CommentController::class, 'store'])->name('comments.store');
-
-
-// Route::get('/', function () {
-//     return view('welcome');
-// });
-
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
-
-Route::middleware('auth')->group(function () {
-    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-});
 
 require __DIR__.'/auth.php';
